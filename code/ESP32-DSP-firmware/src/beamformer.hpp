aa31da4c710432aa7f1edaba49797aa96e3c5047
Oliver <oliver.jyvasjarvi@aalto.fi>
2023-11-24 15:10:28 +0200

    beamforming algorithm

--- code/ESP32-DSP-firmware/src/beamformer.hpp
+++ code/ESP32-DSP-firmware/src/beamformer.hpp
@@ -1,25 +1,56 @@
-/**
- * @file beamformer.hpp
- * @authors Markus Lång
- * @brief Beam former class to perform beam forming algorithm on DSP bugger
- * @version 0.1
- * @date 2023-11-13
- * 
- */
+#include <iostream>
+#include <cmath>
+#include "buffer.h"
+#include "playbackQueue.h"
 
-
-
-class BeamFormer {
+class CircularArrayBeamformer {
 private:
-    /* data */
+    int numSensors;
+    double Sensorarrayradius;
+    double* sensorPositions;
+    playbackQueue* Playback;
+
 public:
-    BeamFormer(/* args */);
-    ~BeamFormer();
+    CircularArrayBeamformer() : numSensors(6), Sensorarrayradius(16.0) {
+        sensorPositions = new double[numSensors];
+        
+        // Initialize sensor positions evenly around the circle
+        for (int i = 0; i < numSensors; ++i) {
+            sensorPositions[i] = i * (360.0 / numSensors);  // Assuming angles are in degrees
+        }
+    }
+
+    ~CircularArrayBeamformer() {
+        delete[] sensorPositions;
+    }
+
+    // Calculate the steering vector for a given angle
+    double* calculateSteeringVector(double angle) {
+        double* steeringVector = new double[numSensors];
+        for (int i = 0; i < numSensors; ++i) {
+            double angleDifference = angle - sensorPositions[i];
+            // Convert angle to radians
+            double angleInRadians = angleDifference * PI / 180.0;
+            steeringVector[i] = std::cos(angleInRadians);
+        }
+        return steeringVector;
+    }
+
+    // Perform delay-and-sum beamforming on a Buffer
+    void beamform(Buffer& buffer, double angle) {
+        int xDim = buffer.getXDim();
+        int yDim = buffer.getYDim();
+
+        double* steeringVector = calculateSteeringVector(angle);
+
+        for (int i = 0; i < xDim; ++i) {
+            double sum = 0.0;
+            for (int j = 0; j < yDim; ++j) {
+                sum += buffer.getValue(i, j) * steeringVector[j];
+            }
+            Playback ->pushBack(sum);
+        }
+
+        delete[] steeringVector;
+    }
 };
-
-
-/**
- * @todo Beam forming
- * 
- */
-
